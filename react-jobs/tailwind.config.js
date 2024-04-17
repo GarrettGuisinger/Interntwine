cd6ee74286bd52e15774154fa1dcef9762ad1819
Harviiep <hporcadas6961@sdsu.edu>
2024-04-16 21:09:28 -0700

    Added tailwind config file to project

--- react-jobs/tailwind.config.js
+++ react-jobs/tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    './pages/**/*.{html,js}',
-  './components/**/*.{html,js}',
-],
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
