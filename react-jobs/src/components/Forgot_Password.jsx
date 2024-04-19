cfca4d37f7639d3ee7143dc76638c08ed4ae8507
Harvie Allen Porcadas <hporcadas6961@sdsu.edu>
2024-04-18 20:41:40 -0700

    Finished Forgot Password Page

--- react-jobs/src/components/Forgot_Password.jsx
+++ react-jobs/src/components/Forgot_Password.jsx
@@ -9,11 +9,27 @@ const Forgot_Password = () => {
             Forgot Password
         </h1>
 
-        <div className='text-center'>
-            <form>
+        <div className='text-center rounded-lg'>
 
-            </form>
+        <form className='pb-4'>
+            <label for = "Username:">Email:</label><br/>
+            <input type = "text" class = "shadow appearance-none border rounded-lg py-2 px-0 text-gray-700 leading-tight focus:outline-none focus:shadow-xl" id = "Username"/>
+    
+        </form>
+
+        <form className='pb-4'>
+            <label for = "Password:">Enter New Password:</label><br/>
+            <input type = "text" class = "shadow appearance-none border rounded-lg py-2 px-0 text-gray-700 leading-tight focus:outline-none focus:shadow-xl" id = "Username"/>
+        </form>
+
         </div>
+
+        <div className='flex justify-center'>
+            <button  button type = "button" className = "text-white font-bold py-2 px-10 rounded-lg bg-green-600 hover:bg-green-500 focus:shadow-xl">
+                Sign Up
+            </button>
+        </div>
+        
     </body>
     </>
   )
