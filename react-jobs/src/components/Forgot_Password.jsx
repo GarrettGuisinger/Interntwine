35142537db71744839766b535c68e4478473e532
Harvie Allen Porcadas <hporcadas6961@sdsu.edu>
2024-04-20 22:48:56 -0700

    fixed issues with landing page and started adding changes to password recovery page

--- react-jobs/src/components/Forgot_Password.jsx
+++ react-jobs/src/components/Forgot_Password.jsx
@@ -1,36 +1,12 @@
 import React from 'react'
+import Forms from './form'
 
 const Forgot_Password = () => {
   return (
     <>
-    <body className='min-h-screen bg-gray-400'>
-
-        <h1 className='flex justify-center text-5xl font-bold py-20'>
-            Forgot Password
-        </h1>
-
-        <div className='text-center rounded-lg'>
-
-        <form className='pb-4'>
-            <label for = "Username:">Email:</label><br/>
-            <input type = "text" class = "shadow appearance-none border rounded-lg py-2 px-0 text-gray-700 leading-tight focus:outline-none focus:shadow-xl" id = "Username"/>
-    
-        </form>
-
-        <form className='pb-4'>
-            <label for = "Password:">Enter New Password:</label><br/>
-            <input type = "text" class = "shadow appearance-none border rounded-lg py-2 px-0 text-gray-700 leading-tight focus:outline-none focus:shadow-xl" id = "Username"/>
-        </form>
-
-        </div>
-
-        <div className='flex justify-center'>
-            <button  button type = "button" className = "text-white font-bold py-2 px-10 rounded-lg bg-green-600 hover:bg-green-500 focus:shadow-xl">
-                Sign Up
-            </button>
-        </div>
-        
-    </body>
+        <body>
+        <Forms route="api/token/" method="Register"/>
+        </body>
     </>
   )
 }
--- react-jobs/src/components/landing_page.jsx
+++ react-jobs/src/components/landing_page.jsx
@@ -8,14 +8,15 @@ function landing_page() {
     
     <>
     
-    <body className='h-full bg-gray-500'>
-            <div className='flex justify-center items-center mb-10 pt-10'>
+    <body className='h-screen bg-gray-500'>
+
+            <div className='flex justify-center items-center pb-5'>
                 <h1 className='text-5xl text-center font-bold'>
                     Welcome to Interntwine
                 </h1>
             </div>
 
-            <div className='flex justify-center pb-20'>
+            <div className='flex justify-center'>
             <Forms route="api/token/" method="Landing"/>
             </div>
     </body>
