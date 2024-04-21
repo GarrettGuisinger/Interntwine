4fdf80c3e5d20e24e8a048f79adda7d0ff25587c
Harvie Allen Porcadas <hporcadas6961@sdsu.edu>
2024-04-21 09:55:24 -0700

    Fixed layout of the Forgot Password page

--- react-jobs/src/components/Forgot_Password.jsx
+++ react-jobs/src/components/Forgot_Password.jsx
@@ -4,8 +4,10 @@ import Forms from './form'
 const Forgot_Password = () => {
   return (
     <>
-        <body>
-        <Forms route="api/token/" method="Register"/>
+        <body className='min-h-screen bg-gray-500 flex justify-center items-center'>
+            <div className='flex justify-center items-center'>
+                <Forms route="api/token/" method="Forgot"/>
+            </div>
         </body>
     </>
   )
