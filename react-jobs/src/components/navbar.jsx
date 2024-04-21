7dd2f2d792959c89d54fc547beb27ba3b5d2e003
Luiz6194 <lgonzalezbauti4071@sdsu.edu>
2024-04-21 11:19:37 -0700

    fixed nav bar

--- react-jobs/src/components/navbar.jsx
+++ react-jobs/src/components/navbar.jsx
@@ -12,10 +12,9 @@ const navbar = () => {
 
           <nav className='sticky top-0 flex flex-col mt-5 '>
             
-            <Link to="/" className='flex items-center justify-center h-40 px-4 text-gray-700 hover:bg-gray-400 hover:text-white border-b border-gray-700'>Home</Link>
-            <Link to="/Inbox" className='flex items-center justify-center h-40 px-4 text-gray-700 hover:bg-gray-400 hover:text-white border-b border-gray-700'>Inbox</Link>
-            <Link to="/Events" className='flex items-center justify-center h-40 px-4 text-gray-700 hover:bg-gray-400 hover:text-white border-b border-gray-700'>Events</Link>
-            <Link to="/Jobs" className='flex items-center justify-center h-40 px-4 text-gray-700 hover:bg-gray-400 hover:text-white border-b border-gray-700'>Jobs</Link>
+            <Link to="/" className='flex items-center justify-center h-48 px-4 text-gray-700 hover:bg-gray-400 hover:text-white border-b border-gray-700'>Home</Link>
+            <Link to="/Inbox" className='flex items-center justify-center h-48 px-4 text-gray-700 hover:bg-gray-400 hover:text-white border-b border-gray-700'>Inbox</Link>
+            <Link to="/Jobs" className='flex items-center justify-center h-48 px-4 text-gray-700 hover:bg-gray-400 hover:text-white border-b border-gray-700'>Jobs</Link>
             <Link to="/Employers" className='flex items-center justify-center h-48 px-4 text-gray-700 hover:bg-gray-400 hover:text-white border-b border-gray-700' >Employers</Link>
             
             </nav>
