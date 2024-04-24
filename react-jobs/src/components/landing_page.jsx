867d1f91ff68a0adb38f6a9f16153f7cbb1ce3dd
Harvie Allen Porcadas <113260125+Harviiep@users.noreply.github.com>
2024-04-24 16:49:11 -0700

    Create account (#53)
    * Created account creation page
    
    * Second commit of account creation
    
    * Started working on Forgot Password page
    
    * Finished Forgot Password Page
    
    * linking forgot password page and account creation page to landing page
    
    * started working on jobs page
    
    * Finished Jobs Page
    
    * Changes to login form component and landing page lay out
    
    * updated landing page
    
    * fixed issues with landing page and started adding changes to password recovery page
    
    * Fixed layout of the Forgot Password page
    
    * Fixed layout of account creation page
    
    * Fixed layout of jobs page and added links to buttons
    
    * Fixed layout of landing page

--- react-jobs/src/components/landing_page.jsx
+++ react-jobs/src/components/landing_page.jsx
@@ -8,18 +8,15 @@ function landing_page() {
     
     <>
     
-    <body className='h-screen bg-gray-500'>
+    <body className='min-h-screen bg-gray-500 flex justify-center items-center'>
 
-            <div className='flex justify-center items-center pb-5'>
-                <h1 className='text-5xl text-center font-bold'>
-                    Welcome to Interntwine
-                </h1>
-            </div>
+            
 
-            <div className='flex justify-center'>
+           <div>
             <Forms route="api/token/" method="Landing"/>
             </div>
     </body>
+
     </>
     )
 
